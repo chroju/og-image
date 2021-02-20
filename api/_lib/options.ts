6404e41df18e8680cde1734d4d84fddb79c8fdeb
chroju <chroju@users.noreply.github.com>
2021-02-20 13:17:45 +0900

    Add lang config

--- api/_lib/options.ts
+++ api/_lib/options.ts
@@ -1,9 +1,9 @@
 import chrome from 'chrome-aws-lambda';
 const exePath = process.platform === 'win32'
-? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-: process.platform === 'linux'
-? '/usr/bin/google-chrome'
-: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    : process.platform === 'linux'
+        ? '/usr/bin/google-chrome'
+        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 interface Options {
     args: string[];
@@ -15,7 +15,7 @@ export async function getOptions(isDev: boolean) {
     let options: Options;
     if (isDev) {
         options = {
-            args: [],
+            args: ['--lang=ja'],
             executablePath: exePath,
             headless: true
         };
